881689728a787601aaf6ebdc20da9af934f9e751
Diansley Fernandes de Lima <diansleyfl@gmail.com>
2021-04-07 18:13:42 -0300

    Criação classe conta

--- EverisKotlinDeveloper/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ EverisKotlinDeveloper/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,11 +5,14 @@ class Pessoa {
 
     var cpf: String = "123.456.789-10"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome CPF: $cpf"
 }
 
 fun main(){
     val diansley = Pessoa()
 
-    println(diansley.nome)
-    println(diansley.cpf)
+    println(diansley.pessoaInfo())
 }
