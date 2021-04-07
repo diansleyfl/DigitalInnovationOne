935fdf6dd73364c116e1d55dd338866dba2dd86b
Diansley Fernandes de Lima <diansleyfl@gmail.com>
2021-04-07 18:33:40 -0300

    Criação classe banco e teste da mesma

---
+++ EverisKotlinDeveloper/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/testes/BancoTeste.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Banco
+
+fun main() {
+    val digiOneBank = Banco(nome = "DigiOneBank", numero = 12)
+
+    println(digiOneBank.info())
+}
