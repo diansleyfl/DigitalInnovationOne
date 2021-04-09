b2737c0ddd5b110cf689420b97e6d44bae41d308
Diansley Fernandes de Lima <diansleyfl@gmail.com>
2021-04-09 16:46:04 -0300

    Criação classe Logavel e teste da mesma

--- EverisKotlinDeveloper/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
+++ EverisKotlinDeveloper/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
@@ -3,6 +3,9 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Analista
 
 fun main() {
-    val jether = Analista(nome="Jether Rodrigues", cpf = "999.999.999-99", salario = 2000.00)
+    val jether = Analista(
+        nome="Jether Rodrigues",
+        cpf = "999.999.999-99",
+        salario = 2000.00)
     ImprimeRelatorioFuncionario.imprime(jether)
 }
--- EverisKotlinDeveloper/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ EverisKotlinDeveloper/DigiOneBank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,6 +3,12 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val marcos = Gerente(nome="Marcos Alexandre", cpf = "111.111.111-11", salario = 5000.00)
+    val marcos = Gerente(
+        nome="Marcos Alexandre",
+        cpf = "111.111.111-11",
+        salario = 5000.00,
+        senha = "senha123")
     ImprimeRelatorioFuncionario.imprime(marcos)
+
+    TesteAutenticacao().autentica(marcos)
 }
