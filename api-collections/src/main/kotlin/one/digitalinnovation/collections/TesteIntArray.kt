ed8fbb5657c264caeb15a8edd64dc9b4943c1380
Diansley Fernandes de Lima <diansleyfl@gmail.com>
2021-04-13 15:45:25 -0300

    criação de testes de Array de int

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -1,2 +1,41 @@
 package one.digitalinnovation.collections
 
+fun main() {
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 7
+    values[2] = 6
+    values[3] = 3
+    values[4] = 2
+
+    for (valor in values) {
+        print("$valor ")
+    }
+    println("\n" + "===============================")
+
+    values.forEach {
+        print("$it ")
+    }
+    println("\n" + "===============================")
+
+    values.forEach {valor ->
+        print("$valor ")
+    }
+    println("\n" + "===============================")
+
+    for(index in values.indices) {
+        print(values[index])
+    }
+    println("\n" + "===============================")
+
+    values.sort()
+    for(valor in values) {
+        print("$valor ")
+    }
+    println("\n" + "===============================")
+
+
+
+
+}
