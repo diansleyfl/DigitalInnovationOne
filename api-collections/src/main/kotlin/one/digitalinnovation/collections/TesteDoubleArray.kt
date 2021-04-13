bd327cac9759565d5380d5840420a2f9aaf80165
Diansley Fernandes de Lima <diansleyfl@gmail.com>
2021-04-13 16:20:15 -0300

    criação de testes de Array de Double

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteDoubleArray.kt
@@ -0,0 +1,27 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = DoubleArray(3)
+
+    salarios[0] = 1000.00
+    salarios[1] = 3000.00
+    salarios[2] = 500.00
+
+    salarios.forEach { println(it) }
+
+    println("=============")
+
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+
+    println("=============")
+
+    val salarios2 = doubleArrayOf(1500.0, 1250.0, 5000.0)
+    salarios2.sort()
+    salarios2.forEach { println(it) }
+
+    println("=============")
+
+}
